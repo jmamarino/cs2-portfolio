708e8e654d79eb8612fb074e5358c79890cd0fd5
jmamarino <jmamarino@brc.pshs.edu.ph>
2026-09-09 07:55:00 +0800

    Update score_checker.py

--- q1/score_checker.py
+++ q1/score_checker.py
@@ -1,13 +1,8 @@
-#PSHS Student Score Checker
-
-#Ask the user to enter their score
 student_score = int(input("Enter student score: "))
 
-#Range: Check if score is outside the 0-100 boundary
 if student_score < 0 or student_score > 100:
     print("This is an Invalid Score.")
 
-#Determine the performance rating for valid scores
 elif student_score >= 90:
     print("Outstanding")
 elif student_score >= 80:
